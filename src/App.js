71b9b964a41b0b50a1b5f80e57dca18ec22ec6b5
Scdales <28219661+Scdales@users.noreply.github.com>
2022-05-16 21:03:51 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typist from 'react-typist';
 
 import './App.css';
 import 'react-typist/dist/Typist.css';
 
 function App() {
+  const [firstLineFinished, setFirstLineFinished] = useState(false);
+
+    const renderRest = () => {
+      setTimeout(() => {
+        setFirstLineFinished(true);
+      }, 1000);
+    }
+
   return (
     <div className="main-container">
-      <Typist avgTypingDelay={20}>
-        <span>Welcome to the site</span>
+      <Typist avgTypingDelay={50} onTypingDone={() => renderRest()} cursor={{ hideWhenDone: true}}>
+        <span>Welcome.</span>
       </Typist>
+      {/*{firstLineFinished ? (*/}
+      {/*  <div>*/}
+      {/*    <Typist cursor={{ show: false}}><span>Something else.</span></Typist>*/}
+      {/*    <Typist cursor={{ show: false}}><span>Something else again.</span></Typist>*/}
+      {/*  </div>*/}
+      {/*) : null}*/}
+
     </div>
   );
 }
